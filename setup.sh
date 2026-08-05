28b7eb814dc03144418be17ddc2dc4bee68ac8a8
osmandagli <osman.dagli687@gmail.com>
2026-08-05 13:01:40 +0200

    comment out NIC forwarding

--- setup.sh
+++ setup.sh
@@ -80,34 +80,34 @@ done
 #systemctl stop irqbalance
 #systemctl disable irqbalance
 
-<<com
-echo "Setting flow director"
 
-for NIC_IFACE in "${NIC_IFACES[@]}"; do
+#echo "Setting flow director"
+#
+#for NIC_IFACE in "${NIC_IFACES[@]}"; do
+#
+#    # Add the rule to the interface
+#    ethtool -U $NIC_IFACE \
+#            flow-type udp4 \
+#            dst-port $RELAY_PORT \
+#            action $RELAY_CPU
+#
+#    # Check the rule
+#    ethtool -u $NIC_IFACE
+#
+#    # Get all the possible NIC IRQs
+#    NIC_IRQ=$(grep ${NIC_IFACE}-TxRx-${RELAY_CPU}$ /proc/interrupts | awk '{print $1}' | tr -d ':')
+#
+#    if [[ -n "$NIC_IRQ" ]]; then
+#            CPU_MASK=$(printf "%x" $((1 << RELAY_CPU)))
+#            echo "$CPU_MASK" > /proc/irq/$NIC_IRQ/smp_affinity
+#            echo "Pinned IRQ $NIC_IRQ to CPU $RELAY_CPU (mask 0x$CPU_MASK)"
+#    else
+#            echo "WARNING: Could not find IRQ for ${NIC_IFACE}-TxRx-${RELAY_CPU}"
+#            echo "Available IRQs:"
+#            grep "$NIC_IFACE" /proc/interrupts
+#    fi
+#done
 
-    # Add the rule to the interface
-    ethtool -U $NIC_IFACE \
-            flow-type udp4 \
-            dst-port $RELAY_PORT \
-            action $RELAY_CPU
-
-    # Check the rule
-    ethtool -u $NIC_IFACE
-
-    # Get all the possible NIC IRQs
-    NIC_IRQ=$(grep ${NIC_IFACE}-TxRx-${RELAY_CPU}$ /proc/interrupts | awk '{print $1}' | tr -d ':')
-
-    if [[ -n "$NIC_IRQ" ]]; then
-            CPU_MASK=$(printf "%x" $((1 << RELAY_CPU)))
-            echo "$CPU_MASK" > /proc/irq/$NIC_IRQ/smp_affinity
-            echo "Pinned IRQ $NIC_IRQ to CPU $RELAY_CPU (mask 0x$CPU_MASK)"
-    else
-            echo "WARNING: Could not find IRQ for ${NIC_IFACE}-TxRx-${RELAY_CPU}"
-            echo "Available IRQs:"
-            grep "$NIC_IFACE" /proc/interrupts
-    fi
-done
-com
 
 # Download perf
 KERNEL_VERSION=$(uname -r)
